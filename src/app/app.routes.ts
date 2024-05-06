d72772a67c568b533b7bed4a9ecc2c3f9fd6c323
Gustavo de Paula <gdepaula@gmail.com>
2024-05-06 09:18:21 -0300

    Hands on 3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () =>
+      import("./pages/pages.module").then((p) => p.PagesModule),
+  },
+];
