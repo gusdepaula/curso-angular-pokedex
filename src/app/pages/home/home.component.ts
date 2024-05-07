01dc22e93662d333bb6aaab3d04f4767284d64c9
Gustavo de Paula <gdepaula@gmail.com>
2024-05-07 15:53:38 -0300

    Hands on 4

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {}
 }
--- src/app/pages/routing.module.ts
+++ src/app/pages/routing.module.ts
@@ -1,15 +1,17 @@
-import { RouterModule, Routes } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
 import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+//Components
+import { HomeComponent } from "./home/home.component";
 import { DetailsComponent } from "./details/details.component";
 
-export const routes: Routes = [
+const routes: Routes = [
   {
     path: "",
     component: HomeComponent,
   },
   {
-    path: "details",
+    path: "details/:id",
     component: DetailsComponent,
   },
 ];
