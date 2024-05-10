b861b56e78f4085339a04e06d693c2d91302c45e
Gustavo de Paula <gdepaula@gmail.com>
2024-05-10 15:03:27 -0300

    Hands on 7

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -10,9 +10,10 @@ import { SharedModule } from "../shared/shared.module";
 //Pages
 import { HomeComponent } from "./home/home.component";
 import { DetailsComponent } from "./details/details.component";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, RoutingModule, SharedModule],
+  imports: [CommonModule, RoutingModule, SharedModule, HttpClientModule],
 })
 export class PagesModule {}
